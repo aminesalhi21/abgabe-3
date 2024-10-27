bdec1751152465c4435632ab4ade3c0b3e082aa9
aminesalhi21 <amine.salhi123@gmail.com>
2024-10-27 13:34:29 +0100

    Update codewort.java

--- codewort.java
+++ codewort.java
@@ -2,7 +2,7 @@ public class codewort {
     public static void main(String[] args) {
         String url = "https://github.com/aminesalhi21"; 
         String githubUsername = "aminesalhi21";
-        System.out.println("URL to personal feedback page: " + url);
-        System.out.println("GitHub username in the solution: " + githubUsername);
+        System.out.println(url);
+        System.out.println(githubUsername);
     }
 }
